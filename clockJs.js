d74dcb8cefeecfdf9804c6ec2ac3124a77f0f80e
adityabanotra <71438721+adityabanotra@users.noreply.github.com>
2021-03-04 18:50:34 +0530

    Update clockJs.js

--- clockJs.js
+++ clockJs.js
@@ -23,50 +23,50 @@ document.getElementsByClassName("outline")[0].onclick = function () {
         
     }
 
-// const secHand= document.querySelector('.sec');
-// const minHand= document.querySelector('.min');
-// const hourHand= document.querySelector('.hour');
+const secHand= document.querySelector('.sec');
+const minHand= document.querySelector('.min');
+const hourHand= document.querySelector('.hour');
 
-// const digtext= document.querySelector('.digclock');
+const digtext= document.querySelector('.digclock');
 
-// function setdate()
-// {
-//     const now= new Date();
+function setdate()
+{
+    const now= new Date();
 
-//     var seconds = now.getSeconds();
-//     var minutes = now.getMinutes();
-//     var hours = now.getHours();
+    var seconds = now.getSeconds();
+    var minutes = now.getMinutes();
+    var hours = now.getHours();
 
 
-//     var secDegree = (seconds/60)*360 +90;
-//     var minDegree = (minutes/60)*360 +90;
-//     var hourDegree = ((hours%12)/12)*360+90;
+    var secDegree = (seconds/60)*360 +90;
+    var minDegree = (minutes/60)*360 +90;
+    var hourDegree = ((hours%12)/12)*360+90;
     
     
-//      secHand.style.transform = `rotate(${secDegree}deg)`;
-//      minHand.style.transform =  `rotate(${minDegree}deg)`;
-//      hourHand.style.transform =  `rotate(${hourDegree}deg)`;
+     secHand.style.transform = `rotate(${secDegree}deg)`;
+     minHand.style.transform =  `rotate(${minDegree}deg)`;
+     hourHand.style.transform =  `rotate(${hourDegree}deg)`;
      
-//      var ses;
-//      ses= (hours<12) ? "AM" : "PM"
-//      hours=hours%12;
-//      if(hours==0)
-//      hours=12;
+     var ses;
+     ses= (hours<12) ? "AM" : "PM"
+     hours=hours%12;
+     if(hours==0)
+     hours=12;
      
-//      hours=(hours<10) ?"0"+hours : hours;
-//      minutes=(minutes<10) ?"0"+minutes : hours;
-//      seconds=(seconds<10) ?"0"+seconds : seconds;
-//      var time= hours+":"+minutes+":"+seconds+ " "+ses;
+     hours=(hours<10) ?"0"+hours : hours;
+     minutes=(minutes<10) ?"0"+minutes : hours;
+     seconds=(seconds<10) ?"0"+seconds : seconds;
+     var time= hours+":"+minutes+":"+seconds+ " "+ses;
 
-//      //digtext.innerText = "time";
-//      digtext.textContent=time;
+     //digtext.innerText = "time";
+     digtext.textContent=time;
      
-//       //document.getElementsById("boxx").textContent=time;
+      //document.getElementsById("boxx").textContent=time;
 
-//       console.log(seconds);
-//     //   console.log(hours);
+      console.log(seconds);
+    //   console.log(hours);
 
 
-// }
-// setInterval(setdate,1000);
+}
+setInterval(setdate,1000);
 
